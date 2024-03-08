748fbf2eede4be6ebbb5ef80740a1210fd3efb75
ermias alehegn <144139764+ermias5@users.noreply.github.com>
2024-03-08 16:54:00 +0300

    dataStructure

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+// Data Structures: Objects and Arrys
+
+//first index in the array is 0;
+/*let listOfNumbers = [2, 4, 6, 8, 3];
+console.log(listOfNumbers[4 - 2]); //4-2 = 2 index of 2 is 6*/
+
+//Almost all JavaScript values have properties except null and undefiend
+
+null.length // null have no props
+
+/* .(after the dot is the literal name of the property) & [](When using square brackets, the expression between the brackets is evaluated to
+get the property name) are access props in js*/

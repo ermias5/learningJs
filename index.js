cf4080a4bbe4d51dc6fa91b973e5873340cb81af
ermias alehegn <144139764+ermias5@users.noreply.github.com>
2024-03-14 11:00:08 +0300

    learning

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
 // Data Structures: Objects and Arrys
 
 //first index in the array is 0;
+
 /*let listOfNumbers = [2, 4, 6, 8, 3];
 console.log(listOfNumbers[4 - 2]); //4-2 = 2 index of 2 is 6*/
 
 //Almost all JavaScript values have properties except null and undefiend
 
-null.length // null have no props
+//null.length // null have no props
 
-/* .(after the dot is the literal name of the property) & [](When using square brackets, the expression between the brackets is evaluated to
+/* .(after the dot is the literal name of the property) & []((looking for binding)When using square brackets, the expression between the brackets is evaluated to
 get the property name) are access props in js*/
+
+/*let ermias = "ermias";
+
+console.log(ermias.toUpperCase());*/
+
+let list = [4, 6, 3, 7];
+
+list.push(9);
+list.pop(6);
+
+console.log(list.pop());
+
+// objects
+
+/*let squrell = {
+  dal1: "preparation",
+  activities: ["understand project", "set up the project", "ready for work"],
+  number: [2, 6, 0, 5]
+}
+
+console.log(squrell.number);*/
+
+//value and keys
+
+///console.log(Object.keys({ "x": 5, "y": 9, "Z": 8 }));
+
+/*let objectA = { a: 2, b: 6 };
+Object.assign(objectA, { b: 7, c: 9 });
+console.log(objectA)*/
+/*let JOURNAL;
+for (let i = 0; i < JOURNAL.length; i++) {
+  entry = JOURNAL[i];
+  console.log(entry)
+};*/
+
+
+
